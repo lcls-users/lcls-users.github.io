177b6f9be0edba79751253b178045837bd9d93fc
Gabriel <gadorlhiac@gmail.com>
2023-02-06 13:01:19 -0800

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,35 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'lcls-users-docs'
+project = 'LCLS Users'
 copyright = '2023, Frédéric Poitevin, Ariana Peck, Gabriel Dorlhiac'
 author = 'Frédéric Poitevin, Ariana Peck, Gabriel Dorlhiac'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.spinx-doc.org/en/master', None)
+}
 
+intersphinx_disabled_domains = ['std']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+epub_show_urls = 'footnote'
